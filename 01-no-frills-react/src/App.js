3abcff299482dc7a9ecf1610ee42e5b3de4832f5
Max <maxwell.merry@gmail.com>
2026-05-10 20:28:41 -0700

    started react course

--- 01-no-frills-react/src/App.js
+++ 01-no-frills-react/src/App.js
@@ -1,28 +1,27 @@
-const Pizza = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
+// Basic React component without JSX
+
+const Pizza = (props) => { // props allows the component to set properties when it is called
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name), // sets the name property
+        React.createElement("p", {}, props.ingredients), // sets the ingredients
+    ]);
 };
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Padre Gino's"),
-    React.createElement(Pizza, {
-      name: "The Pepperoni Pizza",
-      description: "Mozzarella Cheese, Pepperoni",
-    }),
-    React.createElement(Pizza, {
-      name: "The Hawaiian Pizza",
-      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-    }),
-    React.createElement(Pizza, {
-      name: "The Big Meat Pizza",
-      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
-    }),
-  ]);
-};
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+// class == component
+const App = () => { // this is called a react component, it is a function that returns a React element
+    return React.createElement(  // this is the element returned
+        "div", // Specify the type of element to create, in this case a div
+        {}, // Create a div element
+        [
+            React.createElement("h1", {}, "Padre Gino's"), // Create an h1 element with the text "Padre Gino's"
+            React.createElement(Pizza, { name: "Four Cheese Pizza", ingredients: "Parmesan, Mozzarella, Ricotta, Gorgonazola" }),
+            React.createElement(Pizza, { name: "Pepperoni Pizza", ingredients: "Pepperoni, Mozzarella, Marinara Sauce" }),
+            React.createElement(Pizza, { name: "Vegetarian Pizza", ingredients: "Bell Peppers, Onions, Mushrooms, Black Olives" }),
+            React.createElement(Pizza, { name: "Hawaiian Pizza", ingredients: "Ham, Pineapple, Mozzarella" }),
+        ]);
+};
+// this is choosing where on the html page to render the React component
+const container = document.getElementById("root"); // rendered inside the root id "div" in index.html
+const root = ReactDOM.createRoot(container); // Create a React root using the root element
+root.render(React.createElement(App)); // Render an App component instance into the root element
---
+++ padre-ginos/src/App.js
@@ -0,0 +1,43 @@
+import React from "react"
+import { createRoot } from "react-dom";
+
+const Pizza = (props) => {
+  // props allows the component to set properties when it is called
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name), // sets the name property
+    React.createElement("p", {}, props.ingredients), // sets the ingredients
+  ]);
+};
+
+// class == component
+const App = () => {
+  // this is called a react component, it is a function that returns a React element
+  return React.createElement(
+    // this is the element returned
+    "div", // Specify the type of element to create, in this case a div
+    {}, // Create a div element
+    [
+      React.createElement("h1", {}, "Padre Gino's"), // Create an h1 element with the text "Padre Gino's"
+      React.createElement(Pizza, {
+        name: "Four Cheese Pizza",
+        ingredients: "Parmesan, Mozzarella, Ricotta, Gorgonazola",
+      }),
+      React.createElement(Pizza, {
+        name: "Pepperoni Pizza",
+        ingredients: "Pepperoni, Mozzarella, Marinara Sauce",
+      }),
+      React.createElement(Pizza, {
+        name: "Vegetarian Pizza",
+        ingredients: "Bell Peppers, Onions, Mushrooms, Black Olives",
+      }),
+      React.createElement(Pizza, {
+        name: "Hawaiian Pizza",
+        ingredients: "Ham, Pineapple, Mozzarella",
+      }),
+    ],
+  );
+};
+// this is choosing where on the html page to render the React component
+const container = document.getElementById("root"); // rendered inside the root id "div" in index.html
+const root = createRoot(container); // Create a React root using the root element
+root.render(React.createElement(App)); // Render an App component instance into the root element
